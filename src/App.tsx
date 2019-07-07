1e0a3eb61815edfd7ef9e02a1c71e7c5a1c100a9
Max Glasmacher <maxglasmacher@gmail.com>
2019-07-07 20:08:41 +0200

    color search upgraded

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,61 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const colors = ["red", "green", "blue", "yellow"]
+
+interface AppState {
+  currentSearch: string
+  selectedColor: string
+}
+
+
+class App extends React.Component<any, AppState> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      currentSearch: "",
+      selectedColor: ""
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <p className="flex-box">
+            <div className='flex-box-2'>
+              <input
+                value={this.state.currentSearch}
+                onChange={
+                  e => {
+                    this.setState(
+                      {
+                        currentSearch: e.target.value
+                      }
+                    )
+                  }
+                }
+              />
+              <p>{this.state.selectedColor}</p>
+            </div>
+            {colors.filter(color => color.includes(this.state.currentSearch)).
+              map(color => { return (<p className={color} onClick={
+                () => {
+                  this.setState(
+                    {
+                      selectedColor: color
+                    }
+                  )
+                }
+              }>{color}</p>) })}
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
